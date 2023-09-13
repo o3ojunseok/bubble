98004d9c5146ee5a587050ec65da80a7b0b56c39
o3ojunseok <o3ojunseok@gmail.com>
2023-09-13 17:02:24 +0900

    Feat: start setting

---
+++ src/main/java/com/be/bubble/BubbleApplication.java
@@ -0,0 +1,13 @@
+package com.be.bubble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleApplication.class, args);
+	}
+
+}
