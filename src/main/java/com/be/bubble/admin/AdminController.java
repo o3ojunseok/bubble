31781d52f0d9312bc338f455caf7b0332e8e4cae
o3ojunseok <o3ojunseok@gmail.com>
2023-09-17 10:23:01 +0900

    Feat: Migration ing

--- src/main/java/com/be/bubble/admin/AdminController.java
+++ src/main/java/com/be/bubble/admin/AdminController.java
@@ -2,14 +2,10 @@ package com.be.bubble.admin;
 
 import com.be.bubble.admin.dto.AdminResponseDTO;
 import com.be.bubble.admin.dto.SignUpAdminRequestDTO;
-import com.be.bubble.user.User;
-import com.be.bubble.user.dto.SignUpUserRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,8 +21,7 @@ public class AdminController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AdminResponseDTO> findOneAdmin(@PathVariable("id") Long id) {
-        AdminResponseDTO admin = adminService.findByAdmin(id);
-        return ResponseEntity.status(HttpStatus.OK).body(admin);
+    public AdminResponseDTO findOneAdmin(@PathVariable("id") Long id) {
+        return adminService.findByAdmin(id);
     }
 }
--- src/main/java/com/be/bubble/admin/dto/SignUpAdminRequestDTO.java
+++ src/main/java/com/be/bubble/admin/dto/SignUpAdminRequestDTO.java
@@ -1,6 +1,9 @@
 package com.be.bubble.admin.dto;
 
 import com.be.bubble.admin.Admin;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignUpAdminRequestDTO {
 
+    @NotBlank
+    @Email(message = "이메일 형식 필수임")
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\\\W)(?=\\\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 }
